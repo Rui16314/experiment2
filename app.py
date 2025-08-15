30c57493b0ec41221b43b5f4d5cf23ed75684385
Rui16314 <rui315@yorku.ca>
2025-08-15 15:50:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("coordinator-control-panel-service-account.json")
+cred = credentials.Certificate("experiment2-key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
